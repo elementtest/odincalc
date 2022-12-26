29ada7270e05d2a953592d935753d0cb66c9f03e
elementtest <elementtest7@gmail.com>
2022-12-26 17:15:29 +0800

    start 2nd part

---
+++ 2ndpart/script.js
@@ -0,0 +1,16 @@
+//turn buttons into variables
+
+one = document.querySelector(".flexbox-item-1");
+two = document.querySelector(".flexbox-item-2");
+three = document.querySelector(".flexbox-item-3");
+four = document.querySelector(".flexbox-item-4");
+five = document.querySelector(".flexbox-item-5");
+six = document.querySelector(".flexbox-item-6");
+seven = document.querySelector(".flexbox-item-7");
+eight = document.querySelector(".flexbox-item-8");
+nine = document.querySelector(".flexbox-item-9");
+zero = document.querySelector(".flexbox-item-zero");
+
+one.addEventListener('click', e => {
+	console.log(e);	
+})
