df0a0e96f95cb546ebef55133da8300b6ccd4dd1
elementtest <elementtest7@gmail.com>
2022-12-26 18:27:24 +0800

    add event listeners

--- 2ndpart/script.js
+++ 2ndpart/script.js
@@ -1,5 +1,6 @@
 //turn buttons into variables
-
+calculatorviewport = document.querySelector(".calculatorviewport");
+clear = document.querySelector(".flexbox-item-clear");
 one = document.querySelector(".flexbox-item-1");
 two = document.querySelector(".flexbox-item-2");
 three = document.querySelector(".flexbox-item-3");
@@ -10,7 +11,79 @@ seven = document.querySelector(".flexbox-item-7");
 eight = document.querySelector(".flexbox-item-8");
 nine = document.querySelector(".flexbox-item-9");
 zero = document.querySelector(".flexbox-item-zero");
+x = "0";
+function OneBtn(e) {
+	e.preventDefault();
+	x += one.innerHTML;
+	calculatorviewport.innerHTML = x;	
+}
+function TwoBtn(e) {
+	e.preventDefault();
+	x += two.innerHTML;
+	calculatorviewport.innerHTML = x;	
+}
+function ThreeBtn(e) {
+	e.preventDefault();
+	x += three.innerHTML;
+	calculatorviewport.innerHTML = x;	
+}
+function FourBtn(e) {
+	e.preventDefault();
+	x += four.innerHTML;
+	calculatorviewport.innerHTML = x;	
+}
+function FiveBtn(e) {
+	e.preventDefault();
+	x += five.innerHTML;
+	calculatorviewport.innerHTML = x;	
+}
+function SixBtn(e) {
+	e.preventDefault();
+	x += six.innerHTML;
+	calculatorviewport.innerHTML = x;	
+}
+function SevenBtn(e) {
+	e.preventDefault();
+	x += seven.innerHTML;
+	calculatorviewport.innerHTML = x;	
+}
+function EightBtn(e) {
+	e.preventDefault();
+	x += eight.innerHTML;
+	calculatorviewport.innerHTML = x;	
+}
+function NineBtn(e) {
+	e.preventDefault();
+	x += nine.innerHTML;
+	calculatorviewport.innerHTML = x;	
+}
+function ZeroBtn(e) {
+	e.preventDefault();
+	x += zero.innerHTML;
+	calculatorviewport.innerHTML = x;	
+}
 
-one.addEventListener('click', e => {
-	console.log(e);	
-})
+
+
+
+
+function clearDisplay(e) {
+	e.preventDefault();
+	calculatorviewport.innerHTML = "0";
+	x = "0";
+
+}
+
+one.onclick = OneBtn;
+two.onclick = TwoBtn;
+three.onclick = ThreeBtn;
+four.onclick = FourBtn;
+five.onclick = FiveBtn;
+six.onclick = SixBtn;
+seven.onclick = SevenBtn;
+eight.onclick = EightBtn;
+nine.onclick = NineBtn;
+zero.onclick = ZeroBtn;
+console.log(x);
+
+clear.onclick = clearDisplay;
